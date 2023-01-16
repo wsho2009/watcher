6febeb77295a062baad9efcce8f94c772105b5bd
wsho2009 <wsho2009@gmail.com>
2023-01-16 21:35:36 +0900

    [2023/01/16]更新登録

---
+++ src/TimerTest.java
@@ -0,0 +1,21 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerTest {
+
+	//public TimerTest() {
+	//}
+
+	public static void main(String[] args) {
+        Timer timer = new Timer(); // 今回追加する処理
+        TimerTask task = new TimerTask() {
+            int count = 0;
+            public void run() {
+                // 定期的に実行したい処理
+                count++;
+                System.out.println(count + "回目のタスクが実行されました。");
+            }
+        };
+        timer.scheduleAtFixedRate(task,1000,3000); // 今回追加する処理
+    }
+}
